03673f8b46b5b8fc06b026b6619334403a581b8a
mawuse-tech <akpekemawuse1@gmail.com>
2025-12-09 09:43:41 +0000

    create admin

--- controllers/authControllers/auth.controller.js
+++ controllers/authControllers/auth.controller.js
@@ -44,7 +44,7 @@ export const completeRegistration = async (req, res, next) => {
     }
 };
 
-
+createAdmin()
     //login function
     // export const login = async (req, res, next) => {
     //     const { email, password } = req.body;

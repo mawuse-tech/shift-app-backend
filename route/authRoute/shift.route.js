4da185f789e4bbfc07231c31a7c128fdfb117536
mawuse-tech <akpekemawuse1@gmail.com>
2025-12-13 23:28:42 +0000

    reverse

--- route/authRoute/shift.route.js
+++ route/authRoute/shift.route.js
@@ -7,9 +7,9 @@ const router = Router()
 
 router.post('/', routeProtect, adminOnly, createShift)
 router.get('/allShifts', routeProtect, fetchAllShifts)
-router.delete('/deleteShift/:id', adminOnly, deleteShift)
-router.put('/editShift/:shift_id', adminOnly, editShift)
+router.delete('/deleteShift/:id', deleteShift)
+router.put('/editShift/:shift_id', editShift)
 router.get('/myShift', routeProtect, getMyShifts)
-router.get("/history/:workerId", adminOnly, getWorkerShiftHistory);
+router.get("/history/:workerId", getWorkerShiftHistory);
 
 export default router
--- route/authRoute/workers.route.js
+++ route/authRoute/workers.route.js
@@ -2,11 +2,10 @@ import { Router } from "express";
 import { fetchAllWorkers, removeWorker, updateProfile } from "../../controllers/authControllers/workers.controller.js";
 import { routeProtect } from "../../middleware/routeProtect.middleware.js";
 import { fileUpload } from "../../config/fileUpload.js";
-import { adminOnly } from "../../middleware/adminOnly.middleware.js";
 
 const router = Router()
-router.get('/allWorkers', routeProtect, adminOnly,  fetchAllWorkers)
-router.delete('/removeWorker/:id', adminOnly, removeWorker)
-router.put('/updateProfile', routeProtect, adminOnly, fileUpload.single('image'), updateProfile)
+router.get('/allWorkers', routeProtect, fetchAllWorkers)
+router.delete('/removeWorker/:id', removeWorker)
+router.put('/updateProfile', routeProtect, fileUpload.single('image'), updateProfile)
 
 export default router
